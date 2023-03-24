27e1ec4297674f036a5f2003572eaaed96e1e088
Kolizey_08 <magamedbagiev@gmail.com>
2023-03-24 10:16:55 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./slices/productSlice";
 import cartSlice from "./slices/cartSlice";
-import produktSlice from "./slices/produktSlice";
 import userSlice from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     users: userSlice,
     products: productSlice,
-    produkts: produktSlice,
     cart: cartSlice,
   },
 });
